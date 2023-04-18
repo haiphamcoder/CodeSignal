03551bdb95298836345903862bcdb777c1927f59
Hai Pham <ngochai285nd@gmail.com>
2023-04-18 14:43:46 +0700

    update

--- Intro/P07.ThroughTheFog/problem32_absoluteValuesSumMinimization.cpp
+++ Intro/P07.ThroughTheFog/problem32_absoluteValuesSumMinimization.cpp
@@ -2,4 +2,14 @@
 #include <vector>
 using namespace std;
 
+int solution(vector<int> a)
+{
+    return a[(a.size() - 1) / 2];
+}
 
+int main()
+{
+    vector<int> a = {2, 4, 7};
+    cout << solution(a) << endl;
+    return 0;
+}
