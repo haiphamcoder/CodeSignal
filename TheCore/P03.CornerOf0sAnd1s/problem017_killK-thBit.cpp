023ea989fe21372cc176d1d42148739f10b1dfa3
Hai Pham <ngochai285nd@gmail.com>
2023-05-24 23:53:57 +0700

    update problem017

---
+++ TheCore/P03.CornerOf0sAnd1s/problem017_killK-thBit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int k) {
+    return n & ~(1 << (k - 1));
+}
+
+int main() {
+    cout << solution(37, 3) << endl;
+    return 0;
+}
