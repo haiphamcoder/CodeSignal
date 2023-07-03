aba846b4ef2eb9dd4878ec9248a5288fbb1cb2d0
Hai Pham <ngochai285nd@gmail.com>
2023-07-03 14:09:18 +0700

    update problem032

--- TheCore/P04.LoopTunnel/problem032_rounders.cpp
+++ TheCore/P04.LoopTunnel/problem032_rounders.cpp
@@ -6,21 +6,28 @@ int solution(int n)
 {
     int digit = 0;
     int carry = 1;
-    while (n > 0)
+    while (1)
     {
         digit = n % 10;
         n /= 10;
-        if (digit >= 5 && n > 0)
+        if (n == 0)
+        {
+            break;
+        }
+        if (digit >= 5)
         {
             n++;
         }
         carry *= 10;
     }
-    return digit * carry / 10;
+    return digit * carry;
 }
 
 int main()
 {
     cout << solution(5) << endl;
+    cout << solution(15) << endl;
+    cout << solution(1234) << endl;
+    cout << solution(1445) << endl;
     return 0;
 }
